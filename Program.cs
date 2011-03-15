13fc769457a3b889ddea64f37ce960b32b1b5874
JJ Willette <jwillette@gmail.com>
2011-03-14 22:23:17 -0400

    checkin of vs project...nothing yet

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PresML = DocumentFormat.OpenXml.Presentation;
+using PackML = DocumentFormat.OpenXml.Packaging;
+
+namespace SalesForcePPTX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // lots of code to do cool stuff ;)
+        }
+    }
+}
